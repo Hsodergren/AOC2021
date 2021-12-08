951ddce4dbad8f09a1ca6ad02d080afa74ac06ab
Henrik Södergren <>
2021-12-08 12:14:01 +0100

    Added day 6 part 2

--- day6.ml
+++ day6.ml
@@ -3,35 +3,35 @@ let parse_file file =
   String.split_on_char ',' (Utils.read_file file)
   |> List.map int_of_string
 
-let test = [3;4;3;1;2]
+let rec simulate n init stepf =
+  if n = 0
+  then init
+  else simulate (n-1) (stepf init) stepf
 
-let rec duplicate_p1 n fishes =
-  print_endline (string_of_int n);
+let rec stepf_p1 fishes =
   let rec aux fishes acc =
     match fishes with
     | [] -> acc
     | 0::tl -> aux tl (6::8::acc)
     | hd::tl -> aux tl (hd-1::acc)
   in
-  if n = 0
-  then fishes
-  else duplicate (n-1) (aux fishes [])
+  aux fishes []
 
 module FishMap = Map.Make(Int)
 let init fishes =
-  FishMap.
-  let updatef
+  List.fold_left (fun acc f ->
+      FishMap.update f (function | Some v -> Some (v+1) | None -> Some 1) acc
+    ) FishMap.empty fishes
 
-let rec duplicate_p2 n fishes =
-  print_endline (string_of_int n);
-  let rec aux fishes acc =
-    match fishes with
-    | [] -> acc
-    | 0::tl -> aux tl (6::8::acc)
-    | hd::tl -> aux tl (hd-1::acc)
-  in
-  if n = 0
-  then fishes
-  else duplicate (n-1) (aux fishes [])
+let stepf_p2 fishes =
+  FishMap.fold (fun key value acc ->
+      let add_value = function | Some v -> Some (v+value) | None -> Some value in
+      if key = 0
+      then FishMap.update 6 add_value (FishMap.update 8 add_value acc)
+      else FishMap.update (key-1) add_value acc
+    ) fishes FishMap.empty
 
-let part1 = duplicate 80 test
+let test = parse_file input_file
+
+let part1 = simulate 80 test stepf_p1 |> List.length
+let part2 = simulate 256 (init test) stepf_p2 |> FishMap.bindings |> List.fold_left (fun acc (_,v) -> acc+v) 0
